178cd3f2460dfe3224508922db99ac40a63fe177
iiivanpopov <iiivanpopov999@gmail.com>
2025-11-21 15:07:57 +0200

    add layout to not found page

--- apps/frontend/src/components/not-found-page.tsx
+++ apps/frontend/src/components/not-found-page.tsx
@@ -1,21 +1,24 @@
 import { useNavigate } from '@tanstack/react-router'
 import { I18nText } from './i18n'
+import { Layout } from './layout'
 import { Button } from './ui'
 
 export function NotFoundPage() {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <p className="text-6xl font-bold">
-        404
-      </p>
-      <p className="mt-4 text-5xl font-semibold">
-        <I18nText id="title.page-not-found" />
-      </p>
-      <Button className="mt-4" variant="secondary" onClick={() => navigate({ to: '/' })}>
-        <I18nText id="action.go-home" />
-      </Button>
-    </div>
+    <Layout>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="text-6xl font-bold">
+          404
+        </p>
+        <p className="mt-4 text-5xl font-semibold">
+          <I18nText id="title.page-not-found" />
+        </p>
+        <Button className="mt-4" variant="secondary" onClick={() => navigate({ to: '/' })}>
+          <I18nText id="action.go-home" />
+        </Button>
+      </div>
+    </Layout>
   )
 }
